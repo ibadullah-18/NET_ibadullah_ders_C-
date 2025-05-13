97935cf6739aeab472ff8c0f8150b9d76d2ef3e3
Husey_iz52@itstep.edu.az <Husey_iz52@itstep.edu.az>
2025-05-13 16:23:06 +0400

    lesson 2

---
+++ NET 02. Variables/Program.cs
@@ -0,0 +1,49 @@
+﻿// variables
+//int numb;
+//Console.WriteLine(numb);
+
+// @ simvolun deyishen adinin evvelinden istifade etmek mumkundur
+// adeten reserve olmush keyword- leri deyisenin adi kimi  istifade
+// etdike istifade olunur
+//int @while = 12;
+//Console.WriteLine(@while);
+
+
+// default keywordu defolt deyer vermek yeni 0 deyerini vermek ucundur
+//int number = default;
+//Console.WriteLine(number);
+
+
+// var - C++ daki auto ile eynidir yeni consoldan daxil edilen deyere gore int char float falan olur
+//var variable = Console.ReadKey(); 
+
+
+// interpolastion $""
+//var name = "ibadulla";
+//var surname = "huseynzade";
+//int age = 17;
+//Console.WriteLine(name + " " + surname + " " + age);
+//Console.WriteLine($"I am {name} {surname}. I am {age} years old");
+//Console.WriteLine($"I was born in {2025 - age}");
+
+
+// verbatim @
+// @ \n \t \r \b \a \f \v kimi seyler islemir ve neter yaziram elede cap olunur yeni
+// metinin ortasinda diger setre kecsem kodda capdada kecir
+//Console.WriteLine($"I am {name} {surname}.\nI am {age} years old");
+//Console.WriteLine($@"I am {name} {surname}.
+//I am {age} years old");
+//Console.WriteLine($"I am {name} {surname}. "+
+//                  $"I am {age} years old");
+// Console.Write line yazmasan diger setre kecmir
+//Console.Write($"I am {name} {surname}. ");
+//Console.WriteLine( $"I am {age} years old");                
+
+
+// _ - 1000000 yazanda 1_000_000 yazmaq ucundur yeni kodu oxunaqli etmek ucun senin rahatdigin
+//int numb1 = 1_000_000;
+//Console.WriteLine(numb1);
+
+
+//
+
---
+++ NET 02. input output/Program.cs
@@ -0,0 +1,56 @@
+﻿// output
+//Console.BackgroundColor = ConsoleColor.Red; // arxa reng elave edir
+//Console.WriteLine("HELLO Masalli");
+//Console.BackgroundColor = ConsoleColor.Yellow;
+
+//Console.ReadKey();
+
+//Console.Clear();// ozunnen sonrasini entere qeder temizeyir
+//Console.Write("HELLO");
+//Console.BackgroundColor = ConsoleColor.Blue;
+//Console.Write("World");
+
+
+// consoldan beep diye ses cixarir
+//Console.Beep(250000, 100);
+//Thread.Sleep(1000);
+
+
+// input
+//var data = Console.Read(); // aksi cedvelindeki qarsiligin verecek
+//Console.WriteLine(data); // aksi cedvelindeki qarsiligin verecek
+
+//var str = Console.ReadLine(); // string tipinde alir
+//Console.WriteLine(str); // string tipinde alir
+/*int a = (int)str;*/ // bunu C# da tycasting ede bilmeyecik
+
+// Eded tipli deyerleri input olunmasi
+
+// .Parse() - stringi int tipine cevirir
+//var age = int.Parse(Console.ReadLine());
+//Console.WriteLine(2025 - age);
+
+// Convert.ToInt32() - stringi int tipine cevirir
+//var number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(number * 25);
+
+//.TryParse() - stringi int tipine cevirir ve eger cevirse true deyerini qaytarir
+//int numb;
+//int.TryParse(Console.ReadLine()!, out numb);  
+//Console.WriteLine(numb);
+
+//int.TryParse(Console.ReadLine()!, out int numb); // deyiseni bir basa burda elave edirik
+//Console.WriteLine(numb);  
+
+// .Readkey() - bir simvol alir
+//var key = Console.ReadKey(); // bir simvol alir
+//Console.WriteLine(key);
+
+//while (true)
+//{
+//    var key = Console.ReadKey();
+//    Console.WriteLine();
+//    Console.WriteLine($"key name = { key.Key}");
+//    Console.WriteLine($"key char = {key.KeyChar}");
+//}
+
