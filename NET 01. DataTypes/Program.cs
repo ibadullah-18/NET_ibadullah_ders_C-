000bbf47f6b95a7e9828328d9b41f8c8666349af
Ibadullah <Husey_iz52@itstep.edu.az>
2025-05-13 13:20:12 +0400

    Add project files.

---
+++ NET 01. DataTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+    C# - da butun tipler objekt tipinden toreyib
+    
+value type:
+    - struct - dir
+    - stack-de yaranir - FILO
+    - compare: by value  
+    - copy: value 
+    - new keyword-u olmadan yaradilir
+    - clear: scope bitdikde avtomatik olaraq yaddasdan silinir
+referencs type:
+    - class - dir
+    - Heap-de yaranir
+    - compare: by reference
+    - copy: reference
+    - new keyword-u ile yaradilir
+    - clear - Garbage Collector temizleyir
+*/
+
+
