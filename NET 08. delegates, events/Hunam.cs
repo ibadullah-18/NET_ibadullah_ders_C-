3b5aea5c1a1fafbf924eccbebe175967bd7fcedf
Hüseynzadə İbadulla Ziyeviç <Husey_iz52@itstep.edu.az>
2025-05-29 17:51:09 +0400

    NET 09

---
+++ NET 08. delegates, events/Hunam.cs
@@ -0,0 +1,14 @@
+﻿// Delegates
+// delegate return_datatype delegate_name([parameter_list]);
+
+class Hunam
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public override string ToString()
+    {
+        return $"{Name} - {Age}";
+    }
+
+}
+
