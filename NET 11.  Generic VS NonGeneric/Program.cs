4dc01c935258a745882f7050819bf50ba0aa3daf
Hüseynzadə İbadulla Ziyeviç <Husey_iz52@itstep.edu.az>
2025-06-05 16:22:48 +0400

    NET11.1

---
+++ NET 11.  Generic VS NonGeneric/Program.cs
@@ -0,0 +1,39 @@
+﻿// C# Genric <=> C++ template
+/*
+- Class
+- Sturuct
+- Interface
+- Method
+- Delegate
+- collection
+ */
+
+//Some<int> some = new();
+//some.Foo();
+//Some<double> some2 = new(); //  artiq idediyin tipde olur
+//some.Foo();
+
+//some.Bar("salam", 123); // burdada ikiside ozune aid olan tipde olacaq
+
+
+Some<List<int>> some;
+
+class Some<T>where T: class, IEnumerable<int>, new() // burda sert qoya bilirem where T :
+{
+    public T Property { get; set; }
+    public Some(T property)
+    {
+        Property = property;
+    }
+
+    public void Foo()
+    {
+        Console.WriteLine(Property.GetType());
+    }
+    public T2 Bar<T2,T3>(T2 value1, T3 value2)
+    {
+        Console.WriteLine(value1.GetType());
+        Console.WriteLine(value2.GetType());
+        return value1;
+    }
+}
---
+++ NET 11. LINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿// LINQ - Language Integrated Query
+// Kolleksiyalarla işləmək üçün istifadə olunur.
+/*
+LINQ to Objects 
+LINQ to Dataset 
+LINQ to SQL 
+LINQ to XML
+LINQ to Entities
+PLINQ - Parallel LINQ
+*/
+
+
+int[] arr = { -21, 552, -73, 24, -58, 62, -700, 84, -29, -110 };
+ShowArray(arr);
+IEnumerable<int> query = from i in arr // from neyden gotureceyini gosderir'
+                         select i; // select neyi gotureceyini gosderir
+
+#region from , select
+//ShowArray(query);
+//arr[0] = 3;
+//ShowArray(query);
+#endregion
+
+#region where
+
+query = from i in arr // from neyden gotureceyini gosderir
+        where i % 2 == 0 // where neyi gotureceyini gosderir
+        select i; // select neyi gotureceyini gosderir
+ShowArray(query); // -24 552 24 62 84 -110
+#endregion
+
+
+
+
+void ShowArray(IEnumerable<int> arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
