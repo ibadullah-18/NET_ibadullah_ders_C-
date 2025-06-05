5dc0f1d9469a02364cd2ffecc6f612f18b7a5dca
Hüseynzadə İbadulla Ziyeviç <Husey_iz52@itstep.edu.az>
2025-06-05 16:54:09 +0400

    NET11.2

--- NET 11. LINQ/Program.cs
+++ NET 11. LINQ/Program.cs
@@ -10,7 +10,7 @@ PLINQ - Parallel LINQ
 */
 
 
-int[] arr = { -21, 552, -73, 24, -58, 62, -700, 84, -29, -110 };
+int[] arr = { -21, 552, -73, 98, -58, 62, -700, 84, -29, -110 };
 ShowArray(arr);
 IEnumerable<int> query = from i in arr // from neyden gotureceyini gosderir'
                          select i; // select neyi gotureceyini gosderir
@@ -23,13 +23,50 @@ IEnumerable<int> query = from i in arr // from neyden gotureceyini gosderir'
 
 #region where
 
-query = from i in arr // from neyden gotureceyini gosderir
-        where i % 2 == 0 // where neyi gotureceyini gosderir
-        select i; // select neyi gotureceyini gosderir
-ShowArray(query); // -24 552 24 62 84 -110
+//query = from i in arr // from neyden gotureceyini gosderir
+//        where i % 2 == 0 // where neyi gotureceyini gosderir
+//        select i; // select neyi gotureceyini gosderir
+//ShowArray(query); // -24 552 24 62 84 -110
 #endregion
 
+#region orderby ascending(by default) / descending
+//query = from i in arr  // from neyden gotureceyini gosderir
+//        where i > 0
+//        orderby i descending // orderby nece siraya gotureceyini gosderir
+//        select i; // select neyi gotureceyini gosderir
+//ShowArray(query); 
 
+#endregion
+
+#region group by
+//IEnumerable<IGrouping<int,int>> group = from i in arr
+//            where i > 0
+//            group i by i % 2; // group i by i % 2 - i % 2 ile qruplandirir, 0 ve 1 qrupuna
+//foreach (var item in group)
+//{
+//    Console.WriteLine(item.Key);
+//    foreach (var value in item)
+//    {
+//        Console.WriteLine($"      {value}");
+//    }
+//}
+#endregion
+
+#region into
+//var query_into = from i in arr
+//                 where i > 0
+//                 group i by i % 2 into result // into ile yeni bir ad verilir ve yeni bir sorgu yaradilir
+//                 from j in result // yeni bir sorgu yaradilir
+//                 group j by j % 10;
+//foreach (var item in query_into)
+//{
+//    Console.WriteLine(item.Key);
+//    foreach (var value in item)
+//    {
+//        Console.WriteLine($"      {value}");
+//    }
+//}
+#endregion
 
 
 void ShowArray(IEnumerable<int> arr)
