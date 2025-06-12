6e9d5882d2981956b7ee624f30e4c4c4bc9b18b5
Husey_iz52@itstep.edu.az <Husey_iz52@itstep.edu.az>
2025-06-12 17:43:34 +0400

    NET13.2

---
+++ NET 13. Exceptions/Program.cs
@@ -0,0 +1,54 @@
+﻿// Exceptions
+
+/*
+     Exception
+        - SystemException
+        - ApplicationException
+*/
+
+
+int numb1 = 3;
+int numb2 = 0;
+int result = default;
+
+try
+{
+    result = numb1 / numb2;
+    Console.WriteLine(result);
+}
+
+catch (DivideByZeroException ex) // sifra boluneme exceptionsu
+{
+    Console.WriteLine("Catch in Main - DivideByZeroException");
+    Console.WriteLine();
+    Console.WriteLine($"Message - {ex.Message}"); // Message ne oldugunu soyluyor
+    Console.WriteLine();
+    Console.WriteLine($"StackTrace - {ex.StackTrace}"); // StackTrace, nerede oldugunu soyluyor
+    Console.WriteLine();
+    Console.WriteLine($"Target site - {ex.TargetSite}"); // TargetSite, hangi metot oldugunu soyluyor
+}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("Catch in Main - DivideByZeroException");
+//    Console.WriteLine();
+//    Console.WriteLine($"Message - {ex.Message}"); // Message ne oldugunu soyluyor
+//    Console.WriteLine();
+//    Console.WriteLine($"StackTrace - {ex.StackTrace}"); // StackTrace, nerede oldugunu soyluyor
+//    Console.WriteLine();
+//    Console.WriteLine($"Target site - {ex.TargetSite}"); // TargetSite, hangi metot oldugunu soyluyor
+//}
+
+//catch
+//{
+//    Console.WriteLine("Catch in Main - Any exception");
+
+//}
+finally
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("assalamu aleykum");
+}
+
+
+
+
