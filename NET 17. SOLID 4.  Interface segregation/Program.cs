d74df9f5d3cf53ab9afba11be0bbc1a1254ab18d
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2025-06-24 17:24:50 +0400

    NET17

---
+++ NET 17. SOLID 4.  Interface segregation/Program.cs
@@ -0,0 +1,97 @@
+﻿//Interfae segregation
+
+#region Bad example
+
+//abstract class Animal
+//{
+//    public abstract void Feed(); 
+//    public abstract void Groom(); 
+//}
+
+//class Dog : Animal
+//{
+//    public override void Feed()
+//    {
+//        Console.WriteLine("Dog feed");
+//    }
+//    public override void Groom()
+//    {
+//        Console.WriteLine("Dog groom");
+//    }
+//}
+
+//class Cat: Animal
+//{
+//    public override void Feed()
+//    {
+//        Console.WriteLine("Cat feed");
+//    }
+//    public override void Groom()
+//    {
+//        Console.WriteLine("Cat groom");
+//    }
+//}
+
+//class Tiger : Animal
+//{
+//    public override void Feed()
+//    {
+//        Console.WriteLine("Tiger Feed");
+
+//    }
+//    public override void Groom()
+//    {
+//        Console.WriteLine("Duxun catir sigalla! ");
+//    }
+//}
+
+#endregion
+
+#region Good example
+List<IFeed> feedables = [new Tiger(), new Dog(), new Cat()];
+//List<IGroom> groomables = [new Tiger(), new Dog(), new Cat()];
+//List<Ipet> ipetables = [new Tiger(), new Dog(), new Cat()];
+abstract class Animal
+{
+}
+interface IFeed
+{
+    public void Feed();
+}
+interface IGroom
+{
+    public void Groom();
+}
+interface Ipet : IFeed, IGroom 
+{ }
+class Dog : Animal , IFeed, IGroom
+{
+    public void Feed()
+    {
+        Console.WriteLine("Dog feed");
+    }
+    public void Groom()
+    {
+        Console.WriteLine("Dog groom");
+    }
+}
+class Cat : Animal, IFeed, IGroom, Ipet
+{
+    public void Feed()
+    {
+        Console.WriteLine("Cat feed");
+    }
+    public void Groom()
+    {
+        Console.WriteLine("Cat groom");
+    }
+}
+class Tiger : Animal , IFeed
+{
+    public void Feed()
+    {
+        Console.WriteLine("Tiger Feed");
+
+    }
+}
+#endregion
---
+++ NET 17. SOLID Dependency inversion/Program.cs
@@ -0,0 +1,108 @@
+﻿// Dependency inversion
+Application application = new(new JsonSerialization());
+application.CRUD();
+#region Bad example
+
+//class MSSQL
+//{
+//    // CRUD
+//    public void Cerate() => Console.WriteLine("Crate with MSSQL");
+//    public void Read() => Console.WriteLine("Read with MSSQL");
+//    public void Update() => Console.WriteLine("Update with MSSQL");
+//    public void Delete() => Console.WriteLine("Delete with MSSQL");
+//}
+
+//class MySQL
+//{
+//    public void Cerate() => Console.WriteLine("Crate with MySQL");
+//    public void Read() => Console.WriteLine("Read with MySQL");
+//    public void Update() => Console.WriteLine("Update with MySQL");
+//    public void Delete() => Console.WriteLine("Delete with MySQL");
+//}
+
+//class Postagree
+//{
+//    public void Cerate() => Console.WriteLine("Crate with Postagree");
+//    public void Read() => Console.WriteLine("Read with Postagree");
+//    public void Update() => Console.WriteLine("Update with Postagree");
+//    public void Delete() => Console.WriteLine("Delete with Postagree");
+//}
+
+//class Application
+//{
+//    private Postagree _db;
+
+//    public Application(Postagree db)
+//    {
+//        _db = db;
+//    }
+//    public void CRUD()
+//    {
+//        _db.Cerate();
+//        _db.Read();
+//        _db.Update();
+//        _db.Delete();
+//    }
+//}
+
+#endregion
+
+#region Good exapmle
+interface IDb
+{
+    public void Cerate();
+    public void Read();
+    public void Update();
+    public void Delete();
+}
+
+class MSSQL : IDb
+{
+    // CRUD
+    public void Cerate() => Console.WriteLine("Crate with MSSQL");
+    public void Read() => Console.WriteLine("Read with MSSQL");
+    public void Update() => Console.WriteLine("Update with MSSQL");
+    public void Delete() => Console.WriteLine("Delete with MSSQL");
+}
+
+class MySQL: IDb
+{
+    public void Cerate() => Console.WriteLine("Crate with MySQL");
+    public void Read() => Console.WriteLine("Read with MySQL");
+    public void Update() => Console.WriteLine("Update with MySQL");
+    public void Delete() => Console.WriteLine("Delete with MySQL");
+}
+
+class Postagree : IDb
+{
+    public void Cerate() => Console.WriteLine("Crate with Postagree");
+    public void Read() => Console.WriteLine("Read with Postagree");
+    public void Update() => Console.WriteLine("Update with Postagree");
+    public void Delete() => Console.WriteLine("Delete with Postagree");
+}
+
+class JsonSerialization :   IDb
+{
+    public void Cerate() => Console.WriteLine("Crate with JsonSerialization");
+    public void Read() => Console.WriteLine("Read with JsonSerialization");
+    public void Update() => Console.WriteLine("Update with JsonSerialization");
+    public void Delete() => Console.WriteLine("Delete with JsonSerialization");
+}
+
+class Application
+{
+    private IDb _db;
+
+    public Application(IDb db)
+    {
+        _db = db;
+    }
+    public void CRUD()
+    {
+        _db.Cerate();
+        _db.Read();
+        _db.Update();
+        _db.Delete();
+    }
+}
+#endregion
