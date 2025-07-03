6707f644ddfaca2a9e7528d24b994a03d88c3eea
Husey_iz52@itstep.edu.az <Husey_iz52@itstep.edu.az>
2025-07-03 17:48:57 +0400

    NET20.2

---
+++ NET 20. FlyWeight/Program.cs
@@ -0,0 +1,84 @@
+﻿// FlyWeight
+List<UnitFlyWeight> units = new List<UnitFlyWeight>();
+FlyWeightFactory factory = new FlyWeightFactory();
+
+for (int i = 0; i < 10; i++)
+{
+    //Archer archer = new Archer();
+    //Warrior werrior = new Warrior();
+
+    UnitFlyWeight archer = factory.GetUnit("Archer");
+    UnitFlyWeight werrior = factory.GetUnit("Archer");
+
+
+    Console.WriteLine(archer.GetHashCode());
+    Console.WriteLine(werrior.GetHashCode());
+
+
+    units.Add(archer);
+    units.Add(werrior);
+
+}
+
+abstract class UnitFlyWeight
+{
+    protected string Name;
+    protected int AttackPoint;
+    protected int Health;
+    public abstract void Display();
+}
+
+class Archer : UnitFlyWeight
+{
+    public Archer()
+    {
+        Name = "Archer";
+        AttackPoint = 120;
+        Health = 70;
+    }
+    public override void Display()
+    {
+        Console.WriteLine("Archer created");
+    }
+}
+
+class Warrior : UnitFlyWeight
+{
+    public Warrior()
+    {
+        Name = "Werrior";
+        AttackPoint = 120;
+        Health = 70;
+    }
+    public override void Display()
+    {
+        Console.WriteLine("Werrior created");
+    }
+}
+
+class FlyWeightFactory
+{
+    private Dictionary<string, UnitFlyWeight> _units = new();
+    public UnitFlyWeight GetUnit(string key)
+    {
+        UnitFlyWeight unit = default;
+        if(_units.ContainsKey(key))
+        {
+            unit = _units[key];
+        }
+        else
+        {
+            switch (key)
+            {
+                case "Archer":
+                    unit = new Archer();
+                    break;
+                case "Werrior":
+                    unit = new Warrior();
+                    break;
+            }
+            _units.Add(key, unit);
+        }
+            return unit;
+    }
+}
---
+++ NET 20. Proxy/Program.cs
@@ -0,0 +1,48 @@
+﻿// Proxy pattern
+
+/*
+    1. Protection proxy 
+    2. Virtual proxy (Lazy intilization)
+    3. Remote proxy (Remote object)
+    4. Logging proxy 
+    5. Cashing proxy 
+*/
+
+
+// Protection Proxy Example
+
+using System.Security.Principal;
+
+interface IOperation
+{
+    void Request();
+
+}
+
+// Real subject
+
+class RealOperationSubject : IOperation
+{
+    public void Request()
+    {
+        Console.WriteLine("Some real operation.");
+    }
+}
+
+// Proxy
+
+class OperationProxy : IOperation
+{
+
+    private IOperation _operation { get; set; }
+    private ReaderWriterLock _role { get; set; }
+    public OperationProxy(IOperation operation , int role)
+    {
+        _operation = operation;
+        _role = role;
+    }
+    public void Request()
+    {
+      
+    }
+}
